566072ceb0f190d86523eba44b5b1fc4028a7c79
Codersaurus <huudatvu6139@gmail.com>
2023-03-08 11:00:50 +0700

    acccc

--- petsla/settings.py
+++ petsla/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-^)p+*042feurrgk6#xplp@$g+ns(mmw0$_r16!w2!8-*$rh&a9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -171,9 +171,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
